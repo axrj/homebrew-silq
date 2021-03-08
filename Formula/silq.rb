3fdc5dbbb06f90cb7da18799124d8b8828f32d12
Rajesh Amradi <rajeshamradi@gmail.com>
2021-03-08 22:45:27 +0100

    use Ruby 1.9 hash syntax to keep ci happy

--- Formula/silq.rb
+++ Formula/silq.rb
@@ -2,7 +2,7 @@ class Silq < Formula
   desc "Quantum computing programming language by ETH Zurich"
   homepage "https://silq.ethz.ch/"
   license "BSL-1.0"
-  head "https://github.com/eth-sri/silq", :using => :git, :revision => "096774dc95302aafba58399aa9e147a520968703"
+  head "https://github.com/eth-sri/silq", using: git, revision: "096774dc95302aafba58399aa9e147a520968703"
 
   depends_on "wget" => :build
   depends_on "gnuplot" => :recommended
